8bbfa47b95a838f511fd6c60095e108db9a5d84f
luke <ltaylor255@gmail.com>
2020-05-29 20:42:52 -0500

    Added p10k config file

--- bin/linkdots.sh
+++ bin/linkdots.sh
@@ -11,12 +11,25 @@ fi
 # link zsh configuration
 ln -s ~/etc/zsh/.zshrc ~/.zshrc
 
+# link p10k configuration
+ln -s ~/etc/zsh/.p10k.zsh ~/.p10k.zsh
+
 # link oh-my-zsh configuration
 ln -s ~/etc/zsh/.oh-my-zsh/ ~/.oh-my-zsh
+
+# link alacritty settings
 ln -s ~/etc/alacritty/alacritty.yml ~/.alacritty.yml
+
+# link vim colors and plugins
 ln -s ~/etc/vim/.vim ~/.vim
+
+# link vimrc
 ln -s ~/etc/vim/.vimrc ~/.vimrc
+
+# link git settings
 ln -s ~/etc/git/.gitattributes ~/.gitattributes
 ln -s ~/etc/git/.gitconfig ~/.gitconfig
 ln -s ~/etc/git/.gitignore ~/.gitignore
+
+# link VS Code settings
 ln -s ~/etc/vscode/ ~/.vscode
